d830b250ff8a0838017b73ef681ae1e9081d4f96
Mike Montague <mikem@extrahop.com>
2026-04-10 09:25:01 -0700

    list sorting and filter

--- glusers/list.go
+++ glusers/list.go
@@ -5,11 +5,18 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"sort"
 	"strings"
 	"text/tabwriter"
+	"time"
 
 	gitlab "gitlab.com/gitlab-org/api/client-go"
 )
+
+type sortKey struct {
+	field string
+	desc  bool
+}
 
 func truncateUsername(s string, max int) string {
 	if max <= 0 || len(s) <= max {
@@ -21,6 +28,85 @@ func truncateUsername(s string, max int) string {
 	return s[:max-1] + "…"
 }
 
+func parseRoleFilters(values []string) (map[gitlab.AccessLevelValue]bool, error) {
+	if len(values) == 0 {
+		return nil, nil
+	}
+	filter := make(map[gitlab.AccessLevelValue]bool)
+	for _, value := range values {
+		part := strings.TrimSpace(value)
+		if part == "" {
+			return nil, fmt.Errorf("invalid role filter: empty role")
+		}
+		accessLevel, err := parseAccessLevel(strings.ToLower(part))
+		if err != nil {
+			return nil, err
+		}
+		filter[accessLevel] = true
+	}
+	return filter, nil
+}
+
+func parseListArgs(args []string) (map[gitlab.AccessLevelValue]bool, []sortKey, error) {
+	var roles []string
+	var keys []sortKey
+	for _, arg := range args {
+		arg = strings.TrimSpace(arg)
+		if arg == "" {
+			return nil, nil, fmt.Errorf("invalid list argument: empty value")
+		}
+		switch arg[0] {
+		case '+', '-':
+			if len(arg) == 1 {
+				return nil, nil, fmt.Errorf("invalid sort key: %q", arg)
+			}
+			desc := arg[0] == '-'
+			key := arg[1:]
+			switch key {
+			case "user", "role", "expires":
+				keys = append(keys, sortKey{field: key, desc: desc})
+			default:
+				return nil, nil, fmt.Errorf("invalid sort key %q: must be user, role, or expires", key)
+			}
+		default:
+			roles = append(roles, arg)
+		}
+	}
+	filterSet, err := parseRoleFilters(roles)
+	if err != nil {
+		return nil, nil, err
+	}
+	if len(keys) == 0 {
+		keys = []sortKey{
+			{field: "role", desc: true},
+			{field: "expires", desc: false},
+			{field: "user", desc: false},
+		}
+	}
+	return filterSet, keys, nil
+}
+
+func compareExpires(a, b *gitlab.ISOTime) int {
+	if a == nil && b == nil {
+		return 0
+	}
+	if a == nil {
+		return 1
+	}
+	if b == nil {
+		return -1
+	}
+	at := time.Time(*a)
+	bt := time.Time(*b)
+	if at.Before(bt) {
+		return -1
+	}
+	if at.After(bt) {
+		return 1
+	}
+	return 0
+}
+
 func cmdList(args []string) {
 	fs := flag.NewFlagSet("list", flag.ExitOnError)
 	fs.SetOutput(os.Stderr)
@@ -28,8 +114,11 @@ func cmdList(args []string) {
 	tokenFlag := fs.String("token", "", "personal access token (or $GITLAB_TOKEN)")
 	insecureFlag := fs.Bool("insecure", false, "skip TLS certificate verification")
 	fs.Usage = func() {
-		fmt.Fprintln(os.Stderr, "usage: glusers list [flags] <project>")
+		fmt.Fprintln(os.Stderr, "usage: glusers list [flags] <project> [roles...] [+key|-key]...")
 		fs.PrintDefaults()
+		fmt.Fprintln(os.Stderr, "sort keys: +user -user +role -role +expires -expires")
+		fmt.Fprintln(os.Stderr, "default sort: -role +expires +user")
+		fmt.Fprintln(os.Stderr, "roles: guest reporter developer maintainer owner")
 	}
 	fs.Parse(args) //nolint:errcheck
 
@@ -44,11 +133,17 @@ func cmdList(args []string) {
 		fs.Usage()
 		os.Exit(1)
 	}
-	if fs.NArg() != 1 {
+	if fs.NArg() < 1 {
 		fs.Usage()
 		os.Exit(1)
 	}
 	project := fs.Arg(0)
+	filterSet, sortKeys, err := parseListArgs(fs.Args()[1:])
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "error: %v\n", err)
+		fs.Usage()
+		os.Exit(1)
+	}
 
 	gl, err := newGitLabClient(*urlFlag, *tokenFlag, *insecureFlag)
 	if err != nil {
@@ -71,8 +166,54 @@ func cmdList(args []string) {
 		opts.Page = resp.NextPage
 	}
 
+	if len(filterSet) > 0 {
+		filtered := members[:0]
+		for _, m := range members {
+			if filterSet[m.AccessLevel] {
+				filtered = append(filtered, m)
+			}
+		}
+		members = filtered
+	}
+
+	if len(sortKeys) > 0 && len(members) > 1 {
+		sort.SliceStable(members, func(i, j int) bool {
+			a := members[i]
+			b := members[j]
+			for _, key := range sortKeys {
+				var cmp int
+				switch key.field {
+				case "user":
+					au := strings.ToLower(a.Username)
+					bu := strings.ToLower(b.Username)
+					if au < bu {
+						cmp = -1
+					} else if au > bu {
+						cmp = 1
+					}
+				case "role":
+					if a.AccessLevel < b.AccessLevel {
+						cmp = -1
+					} else if a.AccessLevel > b.AccessLevel {
+						cmp = 1
+					}
+				case "expires":
+					cmp = compareExpires(a.ExpiresAt, b.ExpiresAt)
+				}
+				if cmp == 0 {
+					continue
+				}
+				if key.desc {
+					return cmp > 0
+				}
+				return cmp < 0
+			}
+			return false
+		})
+	}
+
 	w := tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0)
-	fmt.Fprintln(w, "USERNAME\tACCESS LEVEL\tEXPIRES")
+	fmt.Fprintln(w, "USER\tACCESS LEVEL\tEXPIRES")
 	for _, m := range members {
 		expires := ""
 		if m.ExpiresAt != nil {
