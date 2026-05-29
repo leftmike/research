f187744da93a62c2750d9635b8cba6d424827eaf
Mike Montague <mikemon@gmail.com>
2026-05-28 19:21:15 -0700

    pop3t: pop3 specific flags and config

--- pop3t/config.go
+++ pop3t/config.go
@@ -6,18 +6,62 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strconv"
 	"strings"
 
 	"github.com/hashicorp/hcl/v2/hclsimple"
 	"github.com/knadh/go-pop3"
 )
 
-type config struct {
+type pop3Config struct {
 	Host     string `hcl:"host,optional"`
 	Port     int    `hcl:"port,optional"`
 	User     string `hcl:"user,optional"`
 	Password string `hcl:"password,optional"`
 	NoTLS    bool   `hcl:"no_tls,optional"`
+}
+
+type smtpConfig struct {
+	Host     string `hcl:"host,optional"`
+	Port     int    `hcl:"port,optional"`
+	User     string `hcl:"user,optional"`
+	Password string `hcl:"password,optional"`
+	NoTLS    bool   `hcl:"no_tls,optional"`
+}
+
+type config struct {
+	Host     string     `hcl:"host,optional"`
+	Port     int        `hcl:"port,optional"`
+	User     string     `hcl:"user,optional"`
+	Password string     `hcl:"password,optional"`
+	POP3     pop3Config `hcl:"pop3,optional"`
+	SMTP     smtpConfig `hcl:"smtp,optional"`
+}
+
+func (cfg *config) pop3Host() string {
+	if cfg.POP3.Host != "" {
+		return cfg.POP3.Host
+	}
+	return cfg.Host
+}
+
+func (cfg *config) pop3Port() int {
+	if cfg.POP3.Port != 0 {
+		return cfg.POP3.Port
+	}
+	return cfg.Port
+}
+func (cfg *config) pop3User() string {
+	if cfg.POP3.User != "" {
+		return cfg.POP3.User
+	}
+	return cfg.User
+}
+func (cfg *config) pop3Password() string {
+	if cfg.POP3.Password != "" {
+		return cfg.POP3.Password
+	}
+	return cfg.Password
 }
 
 func configFilenames() []string {
@@ -42,16 +86,14 @@ func readConfig(filenames []string) (*config, error) {
 }
 
 func loadConfig(fs *flag.FlagSet, args []string) (*config, []string) {
-	var configFile, host, user, password string
-	var port int
-	var noTLS bool
+	var configFile string
 
 	fs.StringVar(&configFile, "config", "", "HCL config file")
-	fs.StringVar(&host, "host", "", "POP3 server host")
-	fs.IntVar(&port, "port", 0, "POP3 server port")
-	fs.StringVar(&user, "user", "", "username")
-	fs.StringVar(&password, "password", "", "password")
-	fs.BoolVar(&noTLS, "no-tls", false, "disable TLS")
+	fs.String("pop3-host", "", "POP3 server host")
+	fs.Int("pop3-port", 0, "POP3 server port")
+	fs.String("pop3-user", "", "POP3 username")
+	fs.String("pop3-password", "", "POP3 password")
+	fs.Bool("pop3-no-tls", false, "POP3 disable TLS")
 	fs.Parse(args)
 
 	var cfg *config
@@ -75,26 +117,18 @@ func loadConfig(fs *flag.FlagSet, args []string) (*config, []string) {
 
 	fs.Visit(func(f *flag.Flag) {
 		switch f.Name {
-		case "host":
-			cfg.Host = host
-		case "port":
-			cfg.Port = port
-		case "user":
-			cfg.User = user
-		case "password":
-			cfg.Password = password
-		case "no-tls":
-			cfg.NoTLS = noTLS
+		case "pop3-host":
+			cfg.POP3.Host = f.Value.String()
+		case "pop3-port":
+			cfg.POP3.Port, _ = strconv.Atoi(f.Value.String())
+		case "pop3-user":
+			cfg.POP3.User = f.Value.String()
+		case "pop3-password":
+			cfg.POP3.Password = f.Value.String()
+		case "pop3-no-tls":
+			cfg.POP3.NoTLS = (f.Value.String() == "true")
 		}
 	})
-
-	if cfg.Port == 0 {
-		cfg.Port = 995
-	}
-
-	if cfg.Host == "" || cfg.User == "" || cfg.Password == "" {
-		fatal(errors.New("host, user, and password are required via config or flag"))
-	}
 
 	// XXX: if verbose
 	// fmt.Printf("%s:%d %s tls:%v\n", cfg.Host, cfg.Port, cfg.User, !cfg.NoTLS)
@@ -102,18 +136,34 @@ func loadConfig(fs *flag.FlagSet, args []string) (*config, []string) {
 }
 
 func (cfg *config) newConn() *pop3.Conn {
+	host := cfg.pop3Host()
+	user := cfg.pop3User()
+	password := cfg.pop3Password()
+	if host == "" || user == "" || password == "" {
+		fatal(errors.New("host, user, and password are required via config or flag"))
+	}
+
+	port := cfg.pop3Port()
+	if port == 0 {
+		if cfg.POP3.NoTLS {
+			port = 110
+		} else {
+			port = 995
+		}
+	}
+
 	conn, err := pop3.New(pop3.Opt{
-		Host:       cfg.Host,
-		Port:       cfg.Port,
-		TLSEnabled: !cfg.NoTLS,
+		Host:       host,
+		Port:       port,
+		TLSEnabled: !cfg.POP3.NoTLS,
 	}).NewConn()
 	if err != nil {
 		fatal(err)
 	}
 
-	err = conn.Auth(cfg.User, cfg.Password)
-	if err != nil && !strings.Contains(cfg.User, "@") {
-		err = conn.Auth(fmt.Sprintf("%s@%s", cfg.User, cfg.Host), cfg.Password)
+	err = conn.Auth(user, password)
+	if err != nil && !strings.Contains(user, "@") {
+		err = conn.Auth(fmt.Sprintf("%s@%s", user, host), password)
 	}
 	if err != nil {
 		fatal(err)
